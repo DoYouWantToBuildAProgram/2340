0004b458c2d110c19c869bf371778357ccaa980e
cbranson3 <cbranson3@gatech.edu>
2016-02-23 18:18:59 -0500

    More attempts(none of which work)

---
+++ app/src/main/java/com/example/erica/recsfromtechs/Movie.java
@@ -0,0 +1,21 @@
+package com.example.erica.recsfromtechs;
+
+/**
+ * Created by Courtney on 2/23/16.
+ */
+public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String newTitle) {
+        title = newTitle;
+    }
+}
+
