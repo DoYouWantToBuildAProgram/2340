78148415364cff9725dd0d6bdb6c48c17bf32a6b
cbranson3 <cbranson3@gatech.edu>
2016-02-26 12:53:34 -0500

    Merged with Nik's branch so that it now shows movie posters Fully functional, demo'ed for M5

--- app/src/main/java/com/example/erica/recsfromtechs/searchScreen.java
+++ app/src/main/java/com/example/erica/recsfromtechs/searchScreen.java
@@ -1,24 +1,18 @@
 package com.example.erica.recsfromtechs;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -26,10 +20,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class searchScreen extends AppCompatActivity {
 
