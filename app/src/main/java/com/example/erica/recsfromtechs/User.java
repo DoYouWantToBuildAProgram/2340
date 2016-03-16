7279a025c41187abb6383114ae7f731dbaa60837
Erica Chia <echia3@gatech.edu>
2016-03-15 21:01:49 -0400

    some stuff

--- app/src/main/java/com/example/erica/recsfromtechs/User.java
+++ app/src/main/java/com/example/erica/recsfromtechs/User.java
@@ -1,5 +1,6 @@
 package com.example.erica.recsfromtechs;
 
+
 /**
  * Created by Erica on 2/23/2016.
  */
@@ -8,11 +9,15 @@ public class User {
     private String name;
     private String email;
     private String major;
+    private String username;
+    private String password;
 
-    public User(String name, String email, String major) {
+    public User(String name, String email, String major, String username, String password) {
         this.name = name;
         this.email = email;
         this.major = major;
+        this.username = username;
+        this.password = password;
     }
 
     public String getName() {
@@ -27,6 +32,14 @@ public class User {
         return major;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     public void setName(String newName){
         name = newName;
     }
@@ -37,6 +50,14 @@ public class User {
 
     public void setMajor(String newMajor) {
         major = newMajor;
+    }
+
+    public void setUsername(String newUsername) {
+        username = newUsername;
+    }
+
+    public void setPassword(String newPassword) {
+        password = newPassword;
     }
 
     public String toString(){ return name +", "+ email+", "+major;}
